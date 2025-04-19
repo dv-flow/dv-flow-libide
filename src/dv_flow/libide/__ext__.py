748ef6bbc0d77191d9d757b9eddd681d30db9816
Matthew Ballance <matt.ballance@gmail.com>
2025-04-19 19:15:14 +0000

    Initial source
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ src/dv_flow/libide/__ext__.py
@@ -0,0 +1,9 @@
+import os
+
+def dvfm_packages():
+    libide_dir = os.path.dirname(__file__)
+
+    return {
+        "ide": os.path.join(libide_dir, "flow.dv"),
+        "ide.dvt": os.path.join(libide_dir, "dvt_flow.dv")
+    }
---
+++ src/dv_flow/libide/dvt_create_filelist.py
@@ -0,0 +1,11 @@
+
+
+async def CreateFilelist(ctxt, input):
+
+    fp = open()
+
+    for fs in input.inputs:
+
+
+
+    pass
